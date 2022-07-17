aee0c0a6358a3917467a731078e163a0a0d9f3c8
Username <daniellesmikle@Danielles-Air.home>
2022-07-17 10:12:54 -0400

    routing

--- piggy/urls.py
+++ piggy/urls.py
@@ -3,21 +3,21 @@ from . import views
 
 
 urlpatterns = [
-    path('rooms', views.room_list, name='room_list'),
+    path('rooms/', views.room_list, name='room_list'),
     path('rooms/<int:pk>', views.room_detail, name='room_detail'),
     # path('rooms/new', views.room_create, name='room_create'),
     path('rooms/new', views.RoomCreate.as_view(), name='room_create'),
     path('rooms/<int:pk>/update', views.RoomUpdate.as_view(), name='room_update'),
     path('rooms/<int:pk>/delete', views.RoomDelete.as_view(), name='room_delete'),
    
-    path('scholars', views.scholar_list, name='scholar_list'),
+    path('scholars/', views.scholar_list, name='scholar_list'),
     path('scholars/<int:pk>', views.scholar_detail, name='scholar_detail'),
     # path('scholars/new', views.scholar_create, name='scholar_create'),
     path('scholars/new', views.ScholarCreate.as_view(), name='scholar_create'),
     path('scholars/<int:pk>/update', views.ScholarUpdate.as_view(), name='scholar_update'),
     path('scholars/<int:pk>/delete', views.ScholarDelete.as_view(), name='scholar_delete'),
 
-    path('parents', views.parent_list, name='parent_list'),
+    path('parents/', views.parent_list, name='parent_list'),
     path('parents/<int:pk>', views.parent_detail, name='parent_detail'),
     # path('parents/new', views.parent_create, name='parent_create'),
     path('parents/new', views.ParentCreate.as_view(), name='parent_create'),
